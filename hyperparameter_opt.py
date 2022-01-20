fae3350cf21d640648da438279e17650bad1dd0a
rkharkness <rachaelharkness1@gmail.com>
2022-01-20 09:42:15 +0000

    Change to return best val metric, log training/val metrics & make lr trial selection categorical

--- hyperparameter_opt.py
+++ hyperparameter_opt.py
@@ -6,149 +6,109 @@ import os
 import argparse
 
 import pandas as pd
+import numpy as np
+
 from dataloaders import make_generators
 
 import torch
 from torch import optim
 
-import keras
+import tensorflow.keras as keras
+from tensorflow.keras.optimizers import Adam
+import tensorflow as tf
+
+from ecovnet.ecovnet import ECovNet
+
 from residual_attn.res_attn import AttentionResNet56
+import matplotlib.pyplot as plt
+#from capsnet.covid_caps import CovidCaps
 
-DEVICE = torch.device("cpu")
+model = ECovNet()
 
-#GET THESE INTO A JSON FILE FOR IMPORT ACROSS FILES
-PATH = "/MULTIX/DATA/HOME/covid-19-benchmarking/"
-BATCHSIZE = 12
-CLASSES = 2
-DIR = os.getcwd()
-EPOCHS = 10
-LOG_INTERVAL = 10
-N_TRAIN_EXAMPLES = BATCHSIZE * 30
-N_VALID_EXAMPLES = BATCHSIZE * 10
-MODEL = AttentionResNet56
+def objective(trial, model=model, supervised=True):
+    trial_lr = trial.suggest_categorical("lr", [1e-5, 5e-4, 1e-4, 5e-3, 1e-3])
+   # trial_lr = trial.suggest_float("lr", 1e-5, 1e-1, log=True)
+    optimizer = model.optimizer
 
-class HyperOpt(torch.nn.Module):
-    def __init__(self, model, data, dataloader) -> None:
-        super().__init__()
-        self.model = model
-        self.data = data # dict of train and val
-        self.dataloader = dataloader # dict of train and val
+    optimizer.learning_rate = trial_lr
+    loss_fn = model.loss_fn
+    
+    # make generators
+    fold = 1
+    params = {'batchsize':24, "num_workers":4, "k":fold}
+    train_loader, val_loader, _ = make_generators('keras',train_df, val_df, test_df, params)
+    # create dict of dataloaders
+    dataloader = {'train':train_loader, 'val':val_loader}
 
-    def checks(self, model_type):
-        assert model_type in ['keras', 'pytorch', 'fastai']
+    best_val_loss = np.inf
 
-    def model_builder(self, trial):
+    if model.model_type == 'keras':
+      loss_dict = {'train': [],'val': []}
 
-        m = self.model
+      for epoch in range(10):
+          print(f'epoch - {epoch}')
+          loss_avg = {'train':[],'val':[]}
+          for phase in ['train', 'val']:
+              for batch in dataloader[phase]:
+                  if len(batch) > 1:
+                      batch_x, batch_y = batch # batch_y can be paired image
+                      with tf.GradientTape() as tape:
+                          pred = model(batch_x)
+                          loss = loss_fn(batch_y, pred)
+                  else:
+                      assert supervised == False
+                      ### more here for unsupervised approaches
+                  if phase == 'train':
+                      grad = tape.gradient(loss, model.trainable_variables)
+                      optimizer.apply_gradients(zip(grad, model.trainable_variables))
+                  else:
+                      pred = model.call(batch_x)
+                      loss = loss_fn(batch_y, pred)
 
-        if m.model_type == 'keras':
-            learning_rate =  trial.suggest_float("lr", 1e-5, 1e-1, log=True)
-            opt = m.get_optimizer()
-            loss_fn = m.get_loss_fn()
-            m.compile(optimizer=opt(lr=learning_rate), loss=loss_fn, metrics=['accuracy'])
-            return m
-        else:
-            return m
+                  loss_avg[phase].append(loss)
 
-    def obj(self, trial, optimizer_name):
+              dataloader[phase].on_epoch_end()
+              loss_dict[phase].append(np.mean(loss_avg[phase]))
+          
+          plt.plot(range(len(loss_dict['train'][:epoch])), loss_dict['train'][:epoch], 'r')
+          plt.plot(range(len(loss_dict['val'][:epoch])), loss_dict['val'][:epoch], 'b')
 
-        # Generate the model
-        model = self.model_builder(trial).to(DEVICE)
+          plt.legend(['Training Loss', 'Val Loss'])
+          plt.xlabel('Epoch')
+          plt.ylabel('Loss')
+          plt.savefig(f'/MULTIX/DATA/nccid/{model.model_name}_lr_{trial_lr}_optuna metrics_epoch.png')
 
-        x_train, y_train = self.data['train'][:N_TRAIN_EXAMPLES]
-        x_val, y_val = self.data['val'][:N_VALID_EXAMPLES]
-
-        if self.model_type == 'keras':
-            model.fit(
-                x_train,
-                y_train,
-                validation_data=(x_val, y_val),
-                shuffle=True,
-                batch_size=BATCHSIZE,
-                epochs=EPOCHS,
-                verbose=False,
-            )
-
-            # Evaluate the model accuracy on the validation set.
-            score = model.evaluate(x_val, y_val, verbose=0)
-            return score[0]
-        
-        else:
-            # Generate the optimizers
-            optimizer_name = model.get_optimizer()
-            lr = trial.suggest_float("lr", 1e-5, 1e-1, log=True)
-            optimizer = getattr(optim, optimizer_name)(model.parameters(), lr=lr)
-
-            # Training of the model.
-            for epoch in range(EPOCHS):
-                model.train()
-                for batch_idx, (data, target) in enumerate(self.dataloader['train']):
-                    # Limiting training data for faster epochs.
-                    if batch_idx * BATCHSIZE >= N_TRAIN_EXAMPLES:
-                        break
-
-                    data, target = data.view(data.size(0), -1).to(DEVICE), target.to(DEVICE)
-
-                    optimizer.zero_grad()
-                    output = model(data)
-                    loss_fn = model.get_loss_fn()
-                    loss = loss_fn(output, target)
-                    
-                    loss.backward()
-                    optimizer.step()
-
-                # Validation of the model.
-                model.eval()
-                correct = 0
-                with torch.no_grad():
-                    for batch_idx, (data, target) in enumerate(self.dataloader['val']):
-                        # Limiting validation data.
-                        if batch_idx * BATCHSIZE >= N_VALID_EXAMPLES:
-                            break
-                        data, target = data.view(data.size(0), -1).to(DEVICE), target.to(DEVICE)
-                        output = model(data)
-                        # Get the index of the max log-probability.
-                        pred = output.argmax(dim=1, keepdim=True)
-                        val_loss = loss_fn(output, target)
-                        correct += pred.eq(target.view_as(pred)).sum().item()
-
-            #    accuracy = correct / min(len(self.dataloader['val'].dataset), N_VALID_EXAMPLES)
-
-                trial.report(val_loss, epoch)
-
-                # Handle pruning based on the intermediate value.
-                if trial.should_prune():
-                    raise optuna.exceptions.TrialPruned()
-
-            return val_loss
-
-
+          if loss_dict['val'][epoch] < best_val_loss:
+              best_val_loss = loss_dict['val'][epoch]
+              print(f'best val loss: {best_val_loss}')
+             
+      metric_df = pd.DataFrame.from_dict(loss_dict)
+      metric_df.to_csv(f'/MULTIX/DATA/nccid/{model.model_name}_optuna_metrics_lr_{trial_lr}.csv')
+      
+      return best_val_loss
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Hyperparameter tuning')
-    parser.add_argument('--data_path', default='~/repos/covid-19-benchmarking/nccid_sample.csv', type=str, help='Path to data file')
-    parser.add_argument('--save_dir', type=str)
-    parser.add_argument('--model_name', type=str)
+    parser.add_argument('--data_csv', default='/MULTIX/DATA/nccid/nccid_preprocessed.csv', type=str, help='Path to data file')
+   # parser.add_argument('--save_dir', type=str)
+  #  parser.add_argument('--model_name', type=str)
     args = parser.parse_args()
+    
+    df = pd.read_csv(args.data_csv)
+    
+    mapping = {'negative':0, 'positive':1}
+    
+    df = df[df['xray_status']!=np.nan]
+    df = df.dropna(subset=['xray_status'])
+    
+    df['xray_status'] = df['xray_status'].map(mapping)
+    
+    train_df = df[df['kfold_1'] == "train"]
+    val_df = df[df['kfold_1'] == "val"]
+    test_df = df[df['kfold_1'] == 'test']
 
-    df = pd.read_csv(args.data_path)
-    train_df = df[df[f'kfold_1'] == "train"]
-    val_df = df[df[f'kfold_1'] == "val"]
-    test_df = df[df[f'kfold_1'] == 'test']
-
-    params = {'batchsize':12, 'num_workers':4}
-
-    #make generators
-    train_loader, val_loader, test_loader = make_generators(train_df, val_df, test_df, params)
-    # create dict of dataloaders
-    data = {'train':train_df, 'val': val_df}
-    dataloaders = {'train':train_loader, 'val':val_loader}
-
-    model = AttentionResNet56()
-    h_optimization = HyperOpt(model, data, dataloaders)
-
-    study = optuna.create_study(direction="minimize") # minimize for loss
-    study.optimize(h_optimization, n_trials=100, timeout=600)
+    study = optuna.create_study(study_name=model.model_name, direction="minimize") # minimize for val loss
+    study.optimize(objective, n_trials=10)
 
     pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
     complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
@@ -169,4 +129,4 @@ if __name__ == "__main__":
 
     print(" Saving best params...")
     hp_df = study.trials_dataframe()
-    hp_df.to_csv(args.save_dir + args.model_name + '.csv')
+    hp_df.to_csv('/MULTIX/DATA/nccid/'+ model.model_name + '_optuna' + '.csv')
