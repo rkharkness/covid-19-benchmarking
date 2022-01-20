4bd6a331cb733b1c2432d61a39ac72dffe4514a8
rkharkness <rachaelharkness1@gmail.com>
2022-01-20 09:40:20 +0000

    Make compatible with training script & adapt for 480x480 image size

--- ecovnet/ecovnet.py
+++ ecovnet/ecovnet.py
@@ -1,33 +1,35 @@
 from pandas.core import base
-from keras import layers
-from keras.layers import AveragePooling2D, BatchNormalization
-from keras.layers import GlobalAveragePooling2D,ZeroPadding2D
-from keras.layers import Conv2D
-from keras.layers import Dropout
-from keras.layers import Flatten
-from keras.layers import Dense
-from keras.layers import Input
-from keras.models import Model
-from keras.optimizers import Adam
-from keras.optimizers import SGD
-from keras.applications import DenseNet121
-from keras.applications import MobileNetV2
-from keras.regularizers import l2
-from keras.layers import Activation
-from keras.regularizers import l1_l2
+from tensorflow.keras import layers
+from tensorflow.keras.layers import AveragePooling2D, BatchNormalization
+from tensorflow.keras.layers import GlobalAveragePooling2D,ZeroPadding2D
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.applications import DenseNet121
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.regularizers import l1_l2
 
-from keras import optimizers
+from tensorflow.keras import optimizers
 import efficientnet.tfkeras as enet
 
-from keras.backend import sigmoid
-from keras.utils import get_custom_objects
-from keras.layers import Activation
+from tensorflow.keras.backend import sigmoid
+from tensorflow.keras.utils import get_custom_objects
+from tensorflow.keras.layers import Activation
 
-from keras.callbacks import Callback
-from keras.optimizers import SGD
-from keras import backend
+from tensorflow.keras.callbacks import Callback
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras import backend
 
-from keras.optimizers import CategoricalCrossentropy
+import tensorflow as tf
+from keras import backend as K
+import keras
 
 from math import pi
 from math import cos
@@ -71,6 +73,28 @@ from math import floor
 	
 # 			self.model.save(filename)
 # 			print('>saved snapshot %s, epoch %d' % (filename, epoch))
+class WeightedBCE(keras.losses.Loss):
+  def __init__(self, trial=None):
+    """adapted from: https://stackoverflow.com/questions/46009619/keras-weighted-binary-crossentropy"""            
+    super().__init__()
+    self.trial = trial
+    self.weights = {'0':1.32571275, '1':0.80276873}
+
+  def call(self, y_true, y_pred):
+        # Original binary crossentropy (see losses.py):
+        # K.mean(K.binary_crossentropy(y_true, y_pred), axis=-1)
+        y_true = tf.cast(y_true, tf.float32)
+        y_pred = tf.cast(y_pred, tf.float32)
+
+        # Calculate the binary crossentropy
+        b_ce = K.binary_crossentropy(y_true, y_pred)
+
+        # Apply the weights
+        weight_vector = y_true * self.weights['1'] + (1. - y_true) * self.weights['0']
+        weighted_b_ce = weight_vector * b_ce
+
+        # Return the mean error
+        return K.mean(weighted_b_ce)
 
 class SwishActivation(Activation):
     
@@ -83,40 +107,40 @@ def swish_act(x, beta = 1):
 
 
 get_custom_objects().update({'swish_act': SwishActivation(swish_act)})
+
 class ECovNet(Model):
-    def __init__(self, model_type, optimizer, loss) -> None:
+    #loss_fn = BinaryCrossentropy(from_logits=False)
+    loss_fn = WeightedBCE()
+    optimizer = Adam()
+    model_type = 'keras'
+    model_name = 'ecovnet'
+
+    def __init__(self, trial=None) -> None:
         super().__init__()
-        self.base_model = enet.EfficientNetB1(include_top=False, input_shape=(480,480,3), pooling='avg', weights="imagenet",classes=2)
-        self.batchnorm = BatchNormalization()
+        self.base_model = enet.EfficientNetB1(include_top=False, input_shape=(480,480,3), pooling='avg', weights="imagenet",classes=1)
+        self.batchnorm1 = BatchNormalization()
+        self.batchnorm2 = BatchNormalization()
+
         self.dense1 = Dense(512, kernel_regularizer=l1_l2(l1=1e-5, l2=1e-3))
-        self.activation(swish_act)
+        self.dense2 = Dense(512, kernel_regularizer=l1_l2(l1=1e-5, l2=1e-3))
+
+        self.activation = swish_act 
         self.dropout = Dropout(0.5)
-        self.dense2 = Dense(2, activation="softmax")
+        self.dense3 = Dense(1, activation="sigmoid")
 
     def call(self, input):
      #   base_model = enet.EfficientNetB1(include_top=False, input_shape=(240,240,3), pooling='avg', weights="imagenet",classes=2)
-        x = self.base_model.output(input)
-        x = self.batchnorm(x)
+        x = self.base_model(input)
+
+        x = self.batchnorm1(x)
         x = self.dense1(x)
         x = self.activation(x)
-        x = self.dropout(x)
 
         x = self.dense2(x)
-        x = self.batchnorm(x)
-        x = self.activation(x)
         x = self.dropout(x)
 
-        output = self.dense2(x)
+        x = self.batchnorm2(x)
+        x = self.activation(x)
+
+        output = self.dense3(x)
         return output
-    
-    @staticmethod
-    def get_loss_fn():
-        return CategoricalCrossentropy()
-    
-    @staticmethod
-    def get_optimizer():
-        return Adam()
-    
-    @staticmethod
-    def get_model_type():
-        return 'keras'
