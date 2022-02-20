4dfadcb0264c36ac989ebd4febe81c24f42d77d3
rkharkness <rachaelharkness1@gmail.com>
2022-02-20 19:58:32 +0000

    Fix model training modes

--- models/siamese_net.py
+++ models/siamese_net.py
@@ -1,122 +1,126 @@
 # import required libraries
-from curses.ascii import SI
-import pandas as pd
-import os
-import cv2
-import time
-import torch
-import numpy as np
-import torch.nn as nn
-import torch.utils.data
-import matplotlib.pyplot as plt
-import torch.nn.functional as F
+from tensorflow.keras.applications.vgg16 import VGG16
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Input, Flatten, Lambda, Dropout
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+import tensorflow.keras.backend as K
+import keras
 
-from torchsummary import summary
+class WeightedBCE(keras.losses.Loss):
+  def __init__(self, trial=None):
+    """adapted from: https://stackoverflow.com/questions/46009619/keras-weighted-binary-crossentropy"""            
+    super().__init__()
+    self.trial = trial
+    self.weights = {'0':1.32571275, '1':0.80276873}
 
-from torch.autograd import Variable
+  def call(self, y_true, y_pred):
+        # Original binary crossentropy (see losses.py):
+        # K.mean(K.binary_crossentropy(y_true, y_pred), axis=-1)
+        y_true = tf.cast(y_true, tf.float32)
+        y_pred = tf.cast((y_pred+1e-10), tf.float32)
 
-import pandas as pd
-from tqdm import tqdm
+        # Calculate the binary crossentropy
+        b_ce = K.binary_crossentropy(y_true, y_pred)
 
-import albumentations as A #need version 0.4.6
-from albumentations.pytorch import ToTensorV2
+        # Apply the weights
+        weight_vector = y_true * self.weights['1'] + (1. - y_true) * self.weights['0']
+        weighted_b_ce = weight_vector * b_ce
 
-class ContrastiveLoss(torch.nn.Module):
-    """
-    Contrastive loss function.
-    Based on: http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
-    """
+        # Return the mean error
+        return K.mean(weighted_b_ce)
 
-    def __init__(self, margin=1.25):
-        super(ContrastiveLoss, self).__init__()
-        self.margin = margin
+""" L1 mistance - manhattan """
+def manhattan_distance(vects):
+    x, y = vects
+    return K.sum(K.abs(x-y), axis=1, keepdims=True)
 
-    def forward(self, x0, x1, y):
-        # euclidean distance
-        diff = x0 - x1
-        dist_sq = torch.sum(torch.pow(diff, 2), 1)
-        dist = torch.sqrt(dist_sq)
-        mdist = self.margin - dist
-        dist = torch.clamp(mdist, min=0.0)
-        loss = y * dist_sq + (1 - y) * torch.pow(dist, 2)
-        loss = torch.sum(loss) / 2.0 / x0.size()[0]
-        return loss
+""" L2 distance """
+def euclidean_distance(vects):
+    x, y = vects
+    sum_square = tf.math.reduce_sum(tf.math.square(x - y), axis=1, keepdims=True)
+    return tf.math.sqrt(tf.math.maximum(sum_square, tf.keras.backend.epsilon()))
 
+def loss(margin=1.25):
+    def contrastive_loss(y_true, y_pred):
+        square_pred = tf.math.square(y_pred)
+        margin_square = tf.math.square(tf.math.maximum(margin - (y_pred), 0))
+        return tf.math.reduce_mean((1 - y_true) * square_pred + (y_true) * margin_square)
 
-class Classifier(nn.Module):
-    def __init__(self):
-        super(Classifier, self).__init__()
-        self.conv1 = nn.Conv2d(1, 8, 3)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(8, 16, 3)
-        self.conv3 = nn.Conv2d(16, 32, 3)
-        self.conv4 = nn.Conv2d(32, 48, 3)
-        self.conv5 = nn.Conv2d(48, 32, 3)
-        self.conv6 = nn.Conv2d(32, 16, 3)
+    return contrastive_loss
 
-
-        self.fc1 = nn.Linear(16*6*6, 32)
-        self.fc2 = nn.Linear(32, 1)
-
-        self.dropout = nn.Dropout(p=0.5)
-
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
-        x = self.pool(F.relu(self.conv4(x)))
-        x = self.pool(F.relu(self.conv5(x)))
-        x = F.relu(self.conv6(x))
-        x = x.view(-1, 16*6*6)
-
-        x = F.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = self.fc2(x)
-        return x
-
-class SiameseNetwork(nn.Module):
-    def __init__(self):
+class SiameseNetwork():
+    def __init__(self, pretrained=None, dropout_act=True):
         super(SiameseNetwork, self).__init__()
+        self.model_type= 'keras'
+        self.model_name = 'siamese_net'
+        self.pretrained = pretrained
+        self.dropout_act = dropout_act
+
+        self.supervised = True
+
+        self.optimizer = Adam()
+
+        if self.pretrained:
+            self.lr = 1e-4
+            self.loss_fn = loss(margin=1.25)
+        else:
+            self.lr = 1e-4
+            self.loss_fn = WeightedBCE()
+
+    def build_encoder(self):
+        base_model = VGG16(weights='imagenet', include_top=False, input_shape=(480,480,3))       
+        output = base_model.output
+        x = Flatten()(output)
+        prediction = Dense(1, activation='sigmoid')(x) # pred to train encoder/embedder
         
-        self.cnn1 = nn.Sequential(
-            nn.Conv2d(1, 8, 3),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(8, 16, 3),
-            nn.Conv2d(16, 32, 3),
-            nn.Conv2d(32, 48, 3),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(48, 32, 3),
-            nn.Conv2d(32, 16, 3)
-          )
+        return Model(inputs=[base_model.input], outputs=[prediction])
 
-
-        self.fc1 = nn.Linear(16*6*6, 32)
-        self.fc2 = nn.Linear(32, 1)
-
-        self.dropout = nn.Dropout(p=0.5)
-      
-    def forward_once(self, x):
-        output = self.cnn1(x)
-        print(output.shape)
-        output = output.view(output.size()[0], -1)
-        output = self.fc1(output)
-        return torch.sigmoid(output)
-
-    def forward(self, input1, input2):
-        output1 = self.forward_once(input1)
-        output2 = self.forward_once(input2)
-        dist = torch.abs(output1 - output2) #try fc layer -5120 to 1 instead
-        dist = self.fc2(dist)
-        return torch.sigmoid(dist)
-
+        
     def build_model(self):
-        model = SiameseNetwork()
-        model = {'model':model, 'optimizer':self.optimizer, 'loss_fn':self.loss_fn, 'lr':self.lr,
-        'model_name':self.model_name, 'model_type':self.model_type, 'supervised':self.supervised}
-        return model
+
+        if self.pretrained: # build encoder
+            input_1 = Input((480,480,3))
+            input_2 = Input((480,480,3))
+
+            embedding_network = self.build_encoder()
+            embedding_network.load_weights(self.pretrained)
+            embedding_network.trainable = False
+
+            model = tf.keras.Sequential() 
+            for layer in embedding_network.layers: # might neeed to remove last layer? 
+                model.add(layer) 
+
+            model.add(Flatten(name='flat'))
+            model.add(Dropout(0.5)) # training=self.dropout_act))
+            model.add(Dense(5120, name='den', activation='sigmoid', kernel_regularizer='l2')) 
+
+            output_1 = embedding_network(input_1)
+            output_2 = embedding_network(input_2)
+
+            merge_layer = Lambda(manhattan_distance)([output_1, output_2]) 
+            output_layer = Dense(1, activation="sigmoid")(merge_layer)
+
+            self.model = Model(inputs=[input_1, input_2], outputs=output_layer) 
+                
+        else:
+            self.model = self.build_encoder() 
+        
+        return {'model':self.model, 'optimizer':self.optimizer, 'loss_fn':self.loss_fn, 'lr':self.lr,
+        'model_name':self.model_name, 'model_type':self.model_type, 'supervised':self.supervised, 'pretrained':self.pretrained}
+
+
+
+    def call(self, inputs):
+        output = self.model['model'](inputs)
+        return output
+
 
 if __name__ == "__main__":
     siamese_net = SiameseNetwork()
     model = siamese_net.build_model()
-    print(summary(model))
+
+    for k, v in model.items():
+        print(k, v)
+
+    print(model['model'].summary())
